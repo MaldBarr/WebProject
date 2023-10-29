48c725dfa75e23ee16ac26011388b9a5ed015552
theGadox <gadiale2002@gmail.com>
2023-10-29 02:23:22 -0300

    Integracion del styles login
    solo falta el css de registo que no esta

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,68 +1,71 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import FetchRegiones from "../assets/fetchRegiones.jsx";
+import styles from '/css/'
 
 function Registro() {
     return (
         <>
         <div>
-        <section>
-            <div>
-            <h2>Registrarse</h2>
-            <p>o</p>
-            <div>
-                <Link to={'/Login'}>
-                    <button>
-                    Iniciar sesion
-                    </button>
-                </Link>
-                <Link to={'/InicioPagina'}><button>Inicio</button></Link>
-            </div>
-            </div>
-        </section>
-        <section id="Login">
-            <form id="Registro" noValidate="">
-            <h2>Registrate</h2>
-            <input
-                type="text"
-                placeholder="Nombre de usuario"
-                id="Username"
-                required=""
-            />
-            <p id={1} />
-            <input type="text" placeholder="Rut" id="rut" name="rut" required="" />
-            <p id={2} />
-            <input type="email" placeholder="Email" id="Email" required="" />
-            <p id={3} />
-            <FetchRegiones/>
-            <p id={4} />
-            <input type="text" placeholder="Comuna" id="comuna" required="" />
-            <p id={5} />
+            <section>
+                <div>
+                    <h2>Registrarse</h2>
+                    <p>o</p> 
+                <div>
+                    <Link to={'/Login'}><button>Iniciar sesion</button></Link>
+                    <Link to={'/InicioPagina'}><button>Inicio</button></Link>
+                </div>
+                </div>
+            </section>
+
+            <section id="Login">
+                <form id="Registro" noValidate="">
+                    <h2>Registrate</h2>
+                    <input
+                        type="text"
+                        placeholder="Nombre de usuario"
+                        id="Username"
+                        required=""
+                    />
+                    <p id={1} />
+                    
+                    <input type="text" placeholder="Rut" id="rut" name="rut" required="" />
+                    <p id={2} />
             
-            <input
-                type="password"
-                placeholder="Contraseña"
-                id="password"
-                required=""
-            />
-            <p id={6} />
-            <input
-                type="password"
-                placeholder="Confirmar Contraseña"
-                id="password2"
-                required=""
-            />
-            <p id={7} />
-            <div id="TermYcond">
-                <input type="checkbox" id="check" required="" />
-                <label>
-                <a href="terminosYcondiciones.md">Acepto terminos y condiciones</a>{" "}
-                </label>
-            </div>
-            <p id={8} />
-            <button type="submit">Registrarse</button>
-            </form>
-        </section>
+                    <input type="email" placeholder="Email" id="Email" required="" />
+                    <p id={3} />
+            
+                    <FetchRegiones/>
+                    <p id={4} />
+            
+                    <input type="text" placeholder="Comuna" id="comuna" required="" />
+                    <p id={5} />
+            
+                    <input
+                        type="password"
+                        placeholder="Contraseña"
+                        id="password"
+                        required=""
+                    />
+                    <p id={6} />
+            
+                    <input
+                        type="password"
+                        placeholder="Confirmar Contraseña"
+                        id="password2"
+                        required=""
+                    />
+                    <p id={7} />
+                    
+                    <div id="TermYcond">
+                        <input type="checkbox" id="check" required="" />
+                        <label><a href="terminosYcondiciones.md">Acepto terminos y condiciones</a>{" "}</label>
+                    </div>
+                    <p id={8} />
+            
+                <button type="submit">Registrarse</button>
+                </form>
+            </section>
         </div>
         </>
     );
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,38 +1,43 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import styles from '/css/login.css'
+
 
 function Login(){
     return (
         <>
-        <div>
-        <section id="Login">
-            <form>
-            <h2>Identificate</h2>
-            <p>Usa tu email y contraseña para Identificarte</p>
-            <input type="email" placeholder="Email" id="email" required="" />
-            <p id={1} />
-            <input
-                type="password"
-                placeholder="Contraseña"
-                id="password"
-                required=""
-            />
-            <p id={2} />
-            <button type="submit">Iniciar sesión</button>
+        <div className={styles.body}>
+        
+        <section id="Login" className={styles.Login}>
+            <form className={styles.form}>
+                <h2>Identificate</h2>
+                <p>Usa tu email y contraseña para Identificarte</p>
+                
+                <input type="email" placeholder="Email" id="email" required="" className={styles.input}/>
+                
+                <p id={1} />
+                
+                <input
+                    type="password"
+                    placeholder="Contraseña"
+                    id="password"
+                    required=""
+                    className={styles.input}    
+                />
+                
+                <p id={2} />
+                <button type="submit" className={styles.button}>Iniciar sesión</button>
             </form>
         </section>
-        <section>
+        
+        <section className={styles.section}>
             <div>
-            <h2>Iniciar sesión</h2>
-            <p>o</p>
-            <div>
-                <Link to={'/Registro'}>
-                    <button>
-                    Crear cuenta
-                    </button>
-                </Link>
-                <Link to={'/InicioPagina'}><button>Inicio</button></Link>
-            </div>
+                <h2>Iniciar sesión</h2>
+                <p>o</p>
+                <div>
+                    <Link to={'/Registro'}><button className={styles.button}>Crear cuenta</button></Link>
+                    <Link to={'/InicioPagina'}><button className={styles.button}>Inicio</button></Link>
+                </div>
             </div>
         </section>
         </div>
