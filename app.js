a2d1e0f4e87898280d3535070d021b53f8bd82c3
Carlos Maldonado Barraza <csmbcarlos@gmail.com>
2023-11-26 13:38:20 -0300

    Registro y Login creados
    Dios te salve github copilot (copitol para mi mamá)

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     user: 'root',
     password: '',
     port: 3306,
-    database: 'BaseTree'
+    database: 'BaseUsers'
 });
 connection.connect(function (err) {
     if (err) {
@@ -15,4 +15,29 @@ connection.connect(function (err) {
         return;
     }
     console.log('Conexión establecida' + connection.threadId);
+});
+const configuracion = {
+    hostname: "127.0.0.1",
+    port: 3000
+};
+app.listen(configuracion, () => {
+    console.log('Servidor iniciado en http://localhost:${configuracion.port}');
+});
+app.put("/Registro", (req, res) => {
+    let username = req.body.username;
+    let rut = req.body.rut;
+    let email = req.body.email;
+    let region = req.body.region;
+    let comuna = req.body.comuna;
+    let password = req.body.password;
+    connection.query('INSERT INTO usuarios SET ?,?,?,?,?,?', [username, rut, email, region, comuna, password], function (err, rows, fields) {
+        res.send(JSON.stringify(rows));
+    });
+});
+app.post("/Login", (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    connection.query('SELECT * FROM usuarios WHERE username=? AND password=?', [username, password], function (err, rows, fields) {
+        res.send(JSON.stringify(rows));
+    });
 });
--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     user : 'root',
     password : '',
     port: 3306,
-    database : 'BaseTree'
+    database : 'BaseUsers'
 });
 
 connection.connect(function(err:any) {
@@ -17,3 +17,31 @@ connection.connect(function(err:any) {
     }
     console.log('Conexión establecida' + connection.threadId);
 });
+const configuracion={
+    hostname:"127.0.0.1",
+    port:3000
+}
+app.listen(configuracion,()=>{
+    console.log('Servidor iniciado en http://localhost:${configuracion.port}')
+})
+
+
+app.put("/Registro",(req:any,res:any)=>{
+    let username=req.body.username;
+    let rut=req.body.rut;
+    let email=req.body.email;
+    let region=req.body.region;
+    let comuna=req.body.comuna;
+    let password=req.body.password;
+    connection.query('INSERT INTO usuarios SET ?,?,?,?,?,?',[username,rut,email,region,comuna,password],function(err:any,rows:any,fields:any){
+        res.send(JSON.stringify(rows));
+    });
+})
+
+app.post("/Login",(req:any,res:any)=>{
+    let username=req.body.username;
+    let password=req.body.password;
+    connection.query('SELECT * FROM usuarios WHERE username=? AND password=?',[username,password],function(err:any,rows:any,fields:any){
+        res.send(JSON.stringify(rows));
+    });
+})
